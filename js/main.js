f339698caf517f8d5b88bfb0296000911d3b1880
joebegley15 <joebegley15@gmail.com>
2015-08-10 09:54:07 -0400

    Greated a get request in the ajax of main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-Words.
+  $(document).ready(function(){
+    var sa = 'http://jsonplaceholder.typicode.com';
+    $.ajax({
+      url: sa + '/posts',
+      method: 'GET'
+    }).done(function(data) {
+      console.log(data);
+    });
+  });
